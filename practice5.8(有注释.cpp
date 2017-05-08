5ff70c1b3552568e4ab7bac6080bd0ffc6affa81
myuaggie <920369216@qq.com>
2017-05-08 21:46:01 +0800

    practice5.8（有注释

---
+++ practice5.8(有注释.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+	string fileName;
+	fstream f;
+	
+	cout << "Input file name?";    //open file
+	cin >> fileName;
+	f.open(fileName);
+	while (!f.is_open()) {
+		cout << "Unable to open that file.Try again." << endl;
+		cout << "Input file name? ";
+		cin >> fileName;
+		f.open(fileName);
+	}
+	 
+	int n;     //get n-letter
+	cout << "Number of  letters of suffix?";
+	cin >> n;
+
+	map<string, int> m;    //get map like <suffix,count>
+	string line;
+	while (f >> line) {
+		if (line.size() < n) continue;
+		m[line.substr(line.size() - n, n)]++;
+	}
+
+	vector<string> member;
+	vector<int> value;
+	for (map<string, int>::iterator k = m.begin(); k != m.end(); k++) {
+		member.push_back((*k).first);
+		value.push_back((*k).second);
+	}
+
+	string temp1;
+	int temp2;
+	for (int idx = 0; idx < member.size() - 1; idx++) {  //bubble sort
+		for (int idx2 = 0; idx2 < member.size() - 1 - idx; idx2++)
+		{
+			if (value[idx2] > value[idx2 + 1])
+			{
+
+				temp1 = member[idx2];
+				member[idx2] = member[idx2 + 1];
+				member[idx2 + 1] = temp1;
+				temp2 = value[idx2];
+				value[idx2] = value[idx2 + 1];
+				value[idx2 + 1] = temp2;
+			}
+		}
+	}
+
+	for (int i = member.size() - 1; i > member.size() - 11; i--) { //print top 10
+		cout << member[i] << ":" << m[member[i]] << endl;
+	}
+	cin.get();
+	cin.get();
+
+	return 0;
+}
