0c43e579a86a0996990c54437e928261132aeba8
myuaggie <920369216@qq.com>
2017-10-24 20:29:50 +0800

    phonecat - step 4

---
+++ phonecat/app/phone-list/phone-list.component.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+describe('phoneList',function(){
+    beforeEach(module('phoneList'));
+    describe('PhoneListController', function() {
+        it('should create a `phones` model with 3 phones', inject(function($componentController) {
+        var ctrl = $componentController('phoneList');
+
+        expect(ctrl.phones.length).toBe(3);
+        expect(scope.name).toBe('world');
+    }));
+
+});
+
+})
