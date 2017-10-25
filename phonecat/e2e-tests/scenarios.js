53f3ca0eacf38c4d734ff8da96e84114e07eec52
myuaggie <920369216@qq.com>
2017-10-25 15:01:59 +0800

    phonecat - step 5

--- phonecat/e2e-tests/scenarios.js
+++ phonecat/e2e-tests/scenarios.js
@@ -2,41 +2,26 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('PhoneCat Application', function() {
+    describe('phoneList', function () {
+        beforeEach(function () {
+            browser.get('index.html');
+        });
+        it('should filter the phone list as a user types into the search box', function () {
+            var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+            var query = element(by.model('$ctrl.query'));
 
+            expect(phoneList.count()).toBe(3);
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
+            query.sendKeys('nexus');
+            expect(phoneList.count()).toBe(1);
 
+            query.clear();
+            query.sendKeys('motorola');
+            expect(phoneList.count()).toBe(2);
 
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#!/view1');
+        });
     });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#!/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
+
+
