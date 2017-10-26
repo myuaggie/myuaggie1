f1d87677ac35dfc46208b00fa26a1a8b9c5a1cdf
myuaggie <920369216@qq.com>
2017-10-26 17:54:14 +0800

    phonecat - step 6

--- phonecat/app/phone-list/phone-list.component.spec.js
+++ phonecat/app/phone-list/phone-list.component.spec.js
@@ -2,13 +2,22 @@
 describe('phoneList',function(){
     beforeEach(module('phoneList'));
     describe('PhoneListController', function() {
-        it('should create a `phones` model with 3 phones', inject(function($componentController) {
-        var ctrl = $componentController('phoneList');
+        var ctrl;
+        beforeEach(inject(function($componentController){
+            ctrl=$componentController('phoneList');
+        }));
+        it('should create a `phones` model with 3 phones', function(){
 
         expect(ctrl.phones.length).toBe(3);
-        expect(scope.name).toBe('world');
-    }));
 
-});
+          });
+        it('should set a default value for the `orderProp` model',function(){
+            expect(ctrl.orderProp).toBe('age');
+        });
+        it('should set a value for the `name` model',function(){
+            expect(ctrl.name).toBe('world');
+        });
+
+    });
 
 })
--- phonecat/e2e-tests/scenarios.js
+++ phonecat/e2e-tests/scenarios.js
@@ -10,7 +10,6 @@ describe('PhoneCat Application', function() {
         it('should filter the phone list as a user types into the search box', function () {
             var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
             var query = element(by.model('$ctrl.query'));
-
             expect(phoneList.count()).toBe(3);
 
             query.sendKeys('nexus');
@@ -20,6 +19,31 @@ describe('PhoneCat Application', function() {
             query.sendKeys('motorola');
             expect(phoneList.count()).toBe(2);
 
+        });
+        it('should be possbile to control phone order via the drop-down menu',function(){
+            var queryField=element(by.model('$ctrl.query'));
+            var orderSelect=element(by.model('$ctrl.orderProp'));
+            var nameOption=orderSelect.element(by.css('option[value="name"]'));
+            var phoneNameColumn= element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+
+            function getNames(){
+                return phoneNameColumn.map(function(elem){
+                    return elem.getText();
+                })
+            }
+            queryField.sendKeys('tablet');
+
+            expect(getNames()).toEqual([
+                'Motorola XOOM\u2122 with Wi-Fi',
+                'MOTOROLA XOOM\u2122'
+            ]);
+
+            nameOption.click();
+
+            expect(getNames()).toEqual([
+                'MOTOROLA XOOM\u2122',
+                'Motorola XOOM\u2122 with Wi-Fi'
+            ]);
         });
     });
 });
