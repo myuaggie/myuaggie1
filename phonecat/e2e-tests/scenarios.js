26d1abf5c7ac7dd1c247bad6c432c92eaef5d7d7
myuaggie <920369216@qq.com>
2017-10-28 23:29:52 +0800

    phonecat - step 8

--- phonecat/e2e-tests/scenarios.js
+++ phonecat/e2e-tests/scenarios.js
@@ -45,7 +45,15 @@ describe('PhoneCat Application', function() {
                 'Motorola XOOM\u2122 with Wi-Fi'
             ]);
         });
+        it('should render phone specific links', function() {
+            var query = element(by.model('$ctrl.query'));
+            query.sendKeys('nexus');
+            element.all(by.css('.phones li a')).first().click();
+            expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
+        });
+
     });
+
 });
 
 
