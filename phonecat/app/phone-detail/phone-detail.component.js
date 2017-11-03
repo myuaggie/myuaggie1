9365eb7c370bd93a5444b1acbf3c1eed6517e404
myuaggie <920369216@qq.com>
2017-11-03 17:09:17 +0800

    phonecat - step 12

--- phonecat/app/phone-detail/phone-detail.component.js
+++ phonecat/app/phone-detail/phone-detail.component.js
@@ -5,9 +5,13 @@ component('phoneDetail', {
     controller: ['$http','$routeParams',
         function PhoneDetailController($http,$routeParams) {
             var self =this;
+            self.setImage=function setImage(imageUrl){
+                self.mainImageUrl=imageUrl;
+            };
             $http.get('phones/'+$routeParams.phoneId+'.json').then(function(response){
                 self.phone=response.data;
-            })
+                self.setImage(self.phone.images[0]);
+            });
         }
     ]
 });
